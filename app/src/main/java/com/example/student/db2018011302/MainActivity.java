87635f9913f8a121ba834fb306c5eb13c21b70d6
Shaojung <shaojung@gmail.com>
2018-01-13 14:49:20 +0800

    add 123

--- app/src/main/java/com/example/student/db2018011302/MainActivity.java
+++ app/src/main/java/com/example/student/db2018011302/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 123123
     }
     public void click1(View v)
     {
