72ce3ffec8f98bad956191a59e4c8bc14fe5e6ab
Shaojung <shaojung@gmail.com>
2018-01-13 14:46:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2018011302/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.student.db2018011302;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        Uri uri = Uri.parse("http://tw.yahoo.com");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+    public void click2(View v)
+    {
+        Uri uri = Uri.parse("tel://0229640123");
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void click3(View v)
+    {
+        Uri uri = Uri.parse("geo:0,0?q=巨匠電腦板橋認證中心");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+    public void click4(View v)
+    {
+        Intent it = new Intent();
+        it.setAction(Intent.ACTION_SEND);
+        it.setType("text/plain");
+        it.putExtra(Intent.EXTRA_TEXT, "Hello 你好");
+        startActivity(it);
+    }
+
+}
